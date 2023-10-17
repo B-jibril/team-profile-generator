601c81b0504e431d54f28bda7bd5e12d7e978409
bash <bjibril123@live.com>
2023-10-17 23:31:14 +0100

    added the intern and engineer function so that the user can input data into the terminal, also it generates users input into the html

--- index.js
+++ index.js
@@ -16,14 +16,105 @@ const idList = []
 const teamMembers = []
 
 const appMenu = () => {
+    function buildTeam() {
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+    }
+
+    function addIntern() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "which is your intern name?",
+
+            },
+            {
+                type: "input",
+                name: "internId",
+                message: "which is your engineer ID?",
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "which is your intern email?",
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "which is your intern school?",
+            },
+
+        ]).then(answers => {
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+            teamMembers.push(intern);
+            idList.push(answers.internId);
+            //console.log(intern);
+            createTeam();
+        })
+    }
+
+
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "which is your engineer name?",
+
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "which is your engineer ID?",
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "which is your engineer email?",
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "which is your engineer Github?",
+            },
+
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            teamMembers.push(engineer);
+            idList.push(answers.engineerId);
+            //console.log(engineer);
+            createTeam();
+        })
+    }
+
+
     function createTeam() {
         inquirer.prompt([
             {
                 type: "list",
                 name: "memberChoice",
                 message: "which type of team member would you like to add?",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "i don't want to add any more team members"
+                ]
             }
-        ])
+        ]).then(userChoice => {
+            if (userChoice.memberChoice === "Engineer") {
+                // add Engineer 
+                addEngineer();
+            } else if (userChoice.memberChoice === "Intern") {
+                //add intern
+                addIntern();
+            } else {
+                // build team function 
+                buildTeam();
+            }
+        })
     }
 
 
@@ -59,6 +150,7 @@ const appMenu = () => {
 
         ]).then(answers => {
             const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber)
+            //console.log(manager);
             teamMembers.push(manager);
             idList.push(answers.managerId);
             createTeam();
